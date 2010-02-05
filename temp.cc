6d2eef095573f84b017d5ccce7dcac2bdaec860d
Chris Mansley <cmansley@dice.rutgers.edu>
2010-02-05 14:09:58 -0500

    Remove some files and added some temp code

--- temp.cc
+++ temp.cc
@@ -13,21 +13,35 @@ using namespace std;
 int main()
 {
 
-  Domain *domain = new DDI(0.9);
-  Chopper *chopper = new Chopper(domain, 20, 5);
+  Gaussian *g = new Gaussian();
 
-  Action c;
-  int b;
+  Chopper *c = new Chopper(g,20,20);
 
-  for(int i = 0; i<25; i++) {
+  HOO h(g, c);
 
-    c = chopper->continuousAction(i);
-    b = chopper->discretizeAction(c);
+  State s = g->getInitialState();
 
-    std::cout << i << "\t->\t" << c[0] << " " << c[1] << "\t->\t" << b  << std::endl;
+  double regret = 0.0;
+  double mean = 0.0;
+
+  for(int t=1; t<2; t++) {
+    regret = 0.0;
+    h.clear();
+    for(int i=0; i<5000; i++) {
+      
+      Action a = h.queryAction(false);
+      SARS *sars = g->step(s, a);
+      h.insertAction(a, sars->reward);
+      
+      regret += 1.0 - sars->reward;
+      delete sars;
+    }
+    //std::cout << regret <<std::endl;
+    mean = mean + (regret - mean)/t;
   }
 
-  
+
+  std::cout << mean << std::endl;
 
   return 0;
 }
